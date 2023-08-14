23914eb7d1d6373afc8836c0ecb6ca3270853ccd
Danila Bolshakov <danila1999777@gmail.com>
2023-08-14 18:03:01 +0300

    Added extension and updated URL

--- ByteCoin-Stroryboard/View Controller/ViewController.swift
+++ ByteCoin-Stroryboard/View Controller/ViewController.swift
@@ -7,16 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource {
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
-    }
-    
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet var bitcoinLabel: UILabel!
     @IBOutlet var currentLabel: UILabel!
@@ -25,6 +16,30 @@ class ViewController: UIViewController, UIPickerViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentPicker.dataSource = self
+        currentPicker.delegate = self
+    }
+    
+    //MARK: - Private Properties
+    
+    private var coinManger = CoinManager()
+    
+    //MARK: - func
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManger.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManger.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectCurrent = coinManger.currencyArray[row]
+        coinManger.getSelect(for: selectCurrent)
     }
 }
 
